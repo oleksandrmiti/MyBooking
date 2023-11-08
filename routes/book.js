6b4886b34c7e4d8b26796187c1f20f6764f3fbc8
Oleksandr <infoformiti@gmail.com>
2023-11-08 15:08:38 +0000

    update book.js
    deleted commented code that was wrong

--- routes/book.js
+++ routes/book.js
@@ -7,7 +7,6 @@ router.get('/create', function(req, res, next) {
   res.render('booking.ejs', { title: 'Make your booking.' });
 });
 
-
 // Show created page
 router.post('/create', function(req, res, next) {
   Booking.create(req.body)
@@ -21,30 +20,14 @@ router.post('/create', function(req, res, next) {
 });
 
 // Show modify page
-// router.get('/modify', function(req,res,next){
-//     res.render('modify.ejs', { title: 'Change it.' });
-// });
-
-
 router.get('/modify', async (req, res) => {
     try {
-        const bookings = await Booking.find({}); // Correct the usage of Model.find()
-        res.render('modify.ejs', { bookings, title: 'All Bookings' }); // Pass 'bookings' to the template
+        const bookings = await Booking.find({});
+        res.render('modify.ejs', { bookings, title: 'All Bookings' });
     } catch (err) {
         console.error(err);
         res.status(500).send('An error occurred');
     }
 });
 
-
-// router.get('/modify', (req, res) => {
-//     Booking.find({}, (err, booking) => {
-//         if (err) {
-//             console.error(err);
-//             res.status(500).send('An error occurred');
-//         } else {
-//             res.render('modify.ejs', { booking });
-//         }
-//     });
-// });
 module.exports = router;
